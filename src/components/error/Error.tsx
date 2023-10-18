e614ce0a6f60b74caf6e510d3cc6e67ad51cc614
akhiljoseph <ajpulluvelil@gmail.com>
2023-10-18 14:38:36 +0530

    KOSTLR-5 - Added a skeleton design for page not found screen

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,7 +1,11 @@
-
+import './Error.scss';
 
 export default function Error() {
     return (
-        <p>error works!</p>
+        <div className="error-container">
+            <h2 className="error-message">Sorry, we couldn't find that page!</h2>
+            <h4 className="error-subscript">Please check the page address.</h4>
+            <button className='btn-back'>Take me back to hompage</button>
+        </div>
     )
 }
