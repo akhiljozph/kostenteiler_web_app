708a0241c9b610ef936cee9c26bc0b36100c9d02
akhiljoseph <ajpulluvelil@gmail.com>
2023-11-02 14:21:58 +0530

    KOSTLR-9 - Added component structures.

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
-import App from "../App";
+import App from "./App";
 
 test('demo', () => {
     expect(true).toBe(true)
