0eea7898e2f418f99118d147f85b2eaa12acbfff
akhiljoseph <ajpulluvelil@gmail.com>
2024-01-11 18:03:56 +0530

    KOSTLR-13 - Added store for Kostlr

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,15 @@
 import { Link, Outlet } from 'react-router-dom';
 import './App.scss';
+import { decremented, incremented, store } from '../../store/store';
 
 function App() {
 
   return (
     <div className='kostlr-wrapper'>
-      <button>
+      <button onClick={store.dispatch(decremented)}>
         <Link to={'/login'}>Login</Link>
       </button>
-      <button>
+      <button onClick={store.dispatch(incremented)}>
         <Link to={'/registration'}>Registration</Link>
       </button>
       <Outlet />
