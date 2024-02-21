f347b1c838fb662182b79e12d9f276ed4e65ffca
akhiljoseph <ajpulluvelil@gmail.com>
2024-02-21 17:50:00 +0530

    KSTLR-16 - Added more styles to login page.

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,25 +1,7 @@
+import { loginBoxWrapper, loginContainer } from "./LoginCSS";
+
 function Login() {
 
-    const flexAlignCenter = {
-        'display': 'flex',
-        'alignItems': 'center',
-        'justifyContent': 'center'
-    }
-
-    const loginContainer = {
-        ...flexAlignCenter,
-        'backgroundColor': '#eae9e9',
-        'height': '100vh',
-        'width': '100vw',
-    }
-
-    const loginBoxWrapper = {
-        'height': '44rem',
-        'width': '29rem',
-        'backgroundColor': '#ffffff',
-        'border': 'solid 0.036rem #000',
-        'border-radius': '1rem',
-    }
     return (
         <div style={loginContainer}>
             <div style={loginBoxWrapper}>
