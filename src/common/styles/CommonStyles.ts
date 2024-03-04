910cd700c3964547331ac42882cca52c9fa20034
akhiljoseph <ajpulluvelil@gmail.com>
2024-03-04 15:47:55 +0530

    KOSTLR-34 - Added font-style, font-weight.

--- src/common/styles/CommonStyles.ts
+++ src/common/styles/CommonStyles.ts
@@ -1,27 +1,54 @@
-export const YononeKaffeesatz400 = {
-    'fontFamily': 'Yanone Kaffeesatz',
-    'font-optical-sizing': 'auto',
+export const normalFontStyle = {
+    'fontStyle': 'normal',
+};
+
+export const italicFontStyle = {
+    'fontStyle': 'italic',
+};
+
+export const obliqueFontStyle = {
+    'fontStyle': 'oblique',
+};
+
+export const initialFontStyle = {
+    'fontStyle': 'initial',
+};
+
+export const inheritFontStyle = {
+    'fontStyle': 'inherit',
+};
+
+export const fontWeight100 = {
+    'fontWeight': 100,
+};
+
+export const fontWeight200 = {
+    'fontWeight': 200,
+};
+
+export const fontWeight300 = {
+    'fontWeight': 300,
+};
+
+export const fontWeight400 = {
     'fontWeight': 400,
-    'fontStyle': 'normal',
-}
+};
 
-export const YononeKaffeesatz500 = {
-    'fontFamily': 'Yanone Kaffeesatz',
-    'font-optical-sizing': 'auto',
+export const fontWeight500 = {
     'fontWeight': 500,
-    'fontStyle': 'normal',
-}
+};
 
-export const YononeKaffeesatz600 = {
-    'fontFamily': 'Yanone Kaffeesatz',
-    'font-optical-sizing': 'auto',
+export const fontWeight600 = {
     'fontWeight': 600,
-    'fontStyle': 'normal',
-}
+};
 
-export const YononeKaffeesatz700 = {
+export const fontWeight700 = {
+    'fontWeight': 700,
+};
+
+export const YononeKaffeesatz = {
+    ...normalFontStyle,
+    ...fontWeight500,
     'fontFamily': 'Yanone Kaffeesatz',
     'font-optical-sizing': 'auto',
-    'fontWeight': 700,
-    'fontStyle': 'normal',
 }
