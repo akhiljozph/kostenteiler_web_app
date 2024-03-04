ba85c41e6eaec0f2fc3a4ef92241e404745f039d
akhiljoseph <ajpulluvelil@gmail.com>
2024-03-04 15:36:57 +0530

    KOSTLR-33 - Added more common font classes.

--- src/common/styles/CommonStyles.ts
+++ src/common/styles/CommonStyles.ts
@@ -1,6 +1,27 @@
-export const YononeKaffeesatz = {
+export const YononeKaffeesatz400 = {
+    'fontFamily': 'Yanone Kaffeesatz',
+    'font-optical-sizing': 'auto',
+    'fontWeight': 400,
+    'fontStyle': 'normal',
+}
+
+export const YononeKaffeesatz500 = {
     'fontFamily': 'Yanone Kaffeesatz',
     'font-optical-sizing': 'auto',
     'fontWeight': 500,
+    'fontStyle': 'normal',
+}
+
+export const YononeKaffeesatz600 = {
+    'fontFamily': 'Yanone Kaffeesatz',
+    'font-optical-sizing': 'auto',
+    'fontWeight': 600,
+    'fontStyle': 'normal',
+}
+
+export const YononeKaffeesatz700 = {
+    'fontFamily': 'Yanone Kaffeesatz',
+    'font-optical-sizing': 'auto',
+    'fontWeight': 700,
     'fontStyle': 'normal',
 }
