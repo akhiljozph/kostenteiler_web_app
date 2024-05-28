9c5540ab12cb956aa13686466140c4cb2c4fa8ad
akhiljoseph <ajpulluvelil@gmail.com>
2024-05-28 16:03:14 +0530

    KOSTLR-44 - Added common for post method.

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -11,9 +11,26 @@ export const baseGetValueByParam = (url: string, param: string) => {
     .finally(() => {});
 };
 
-export const baseGetValueByBody = (url: string, body: AxiosRequestConfig<any> | undefined) => {
+export const baseGetValueByBody = (url: string, body: any | undefined) => {
   axios
     .get(baseURL + url, body)
+    .then(() => { })
+    .catch(() => { })
+    .finally(() => { });
+};
+
+export const basePostValueByParam = (url: string, param: string) => {
+  console.log("BaseGetValueByParam", baseURL + url + param);
+  axios
+    .post(baseURL + url + param)
+    .then(() => {})
+    .catch(() => {})
+    .finally(() => {});
+};
+
+export const basePostValueByBody = (url: string, body: any | undefined) => {
+  axios
+    .post(baseURL + url, body)
     .then(() => { })
     .catch(() => { })
     .finally(() => { });
